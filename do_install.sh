22fd5cc9dd7749cc9f805dba3d48a89a82b03106
chajiuqqq <chajiuqqq@gmail.com>
2023-12-25 14:56:59 +0800

    same to last commit

--- do_install.sh
+++ do_install.sh
@@ -13,3 +13,12 @@ rm -rf /usr/local/go && wget https://go.dev/dl/go1.20.12.linux-amd64.tar.gz && t
 export GOPROXY=https://goproxy.io,direct
 go install golang.org/dl/go1.19@latest && go1.19 download && go1.19 build -o ../../bin/qperf
 export PATH=$PATH:$PWD/bin
+
+# install chrome
+sudo apt install wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+ln /usr/bin/google-chrome-stable /usr/bin/google-chrome
+
+# run page loading time tester
+# python3 submod/pageloading-tester/src/main.py bin/chromedriver https://www.baidu.com/ performance.json
