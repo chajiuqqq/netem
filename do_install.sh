b48196f43f2878573a34e04d97ba46f39fe80ff9
chris <chajiuqqq@gmail.com>
2024-03-07 10:03:19 +0800

    update install.sh

--- do_install.sh
+++ do_install.sh
@@ -1,9 +1,8 @@
 git submodule update --init --recursive
 
-# install mininet
-PWD=$(pwd)
-cd submod/mininet/util 
-sh ./install.sh
+# install mininet;should installed in /root
+cd /root && git clone https://github.com/mininet/mininet.git
+./mininet/util/install.sh -a
 
 # install go
 rm -rf /usr/local/go && wget https://go.dev/dl/go1.20.12.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.20.12.linux-amd64.tar.gz && rm go1.20.12.linux-amd64.tar.gz
--- net_gym/sb3_test.py
+++ net_gym/sb3_test.py
@@ -12,7 +12,7 @@ obs = vec_env.reset()
 for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
-    vec_env.render("human")
+    # vec_env.render("human")
     # VecEnv resets automatically
     # if done:
     #   obs = vec_env.reset()
